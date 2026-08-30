425d1324d48ec22fc7e25b6748845d6fc5b8ecc9
rsadikur <sadikurhly@gmail.com>
2026-08-30 07:29:42 +0530

    Rewinded the Logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -74,6 +74,7 @@ export default function Navbar() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16 lg:h-20">
             <Link href="/" className="flex items-center gap-2.5 group">
+              <LogoHorizontal size="md" showTagline={true} />
             </Link>
 
             <div className="hidden lg:flex items-center gap-1">
